20f9fb4dc1b2fe233ed3c1526d996091863af4cf
Diego Puerto <diego23p@hotmail.com>
2020-03-02 13:03:14 -0500

    creación appModule

--- BLUEPRINTS_PART1/src/main/resources/static/js/app.js
+++ BLUEPRINTS_PART1/src/main/resources/static/js/app.js
@@ -1,9 +1,14 @@
-var app = (function () {
+var app =(function(){
+    var author=null;
 
-	return {
-        input: function(author, callback) {
-            callback(null, mockdata[author]);
-        }
-    }
-
+    return {
+        input:function (author2) {
+                  author = author2;
+                  document.getElementById("authorName").innerHTML = author+"'s bluesprints:";
+                },
+         update: function(author){
+            app.input(author);
+            var bp = apimock.getBlueprintsByAuthor(author,null);
+         }
+	};
 })();
